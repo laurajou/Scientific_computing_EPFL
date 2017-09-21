584e54ebe8a767203202e27ca3041a643f34721e
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2017-09-21 16:03:41 +0200

    template exercise from first week

---
+++ work/exercise-template.cpp
@@ -0,0 +1,22 @@
+/*
+ * exercise-template.cpp <--- The name of the source file goes here
+ *
+ * <--- Description of the program goes here.
+ *
+ * Created on: September 02, 2015 <--- Fill in the date here
+ * 	   Author: Davide Forti <davide.forti@epfl.ch> <--- Fill in your name (and e-mail) here
+ */
+
+/*
+ * Any headers you need to include should be specified on the next lines
+ */
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	/*
+	 * The body of the program goes here
+	 */
+    return 0;
+}
+
