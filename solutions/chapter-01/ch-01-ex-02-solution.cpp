8ec23fb502b4f68b819761783e316b813eadb369
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2017-09-29 07:16:24 +0200

    adding the solutions for chapter1

--- solutions/chapter-01/ch-01-ex-02-solution.cpp
+++ solutions/chapter-01/ch-01-ex-02-solution.cpp
@@ -9,21 +9,20 @@
 
 #include <iostream>
 
-int main(int argc, char* argv[])
-{
-	// Read the two numbers from the keyboard
-	std::cout << "Please input the first number. a= ";
-	// Declare an integer variable and read a value from the keyboard
-	int a;
-	std::cin >> a;
-	std::cout << "Please input the second number. b= ";
-	// Same thing for the second number
-	int b;
-	std::cin >> b;
+int main(int argc, char *argv[]) {
+  // Read the two numbers from the keyboard
+  std::cout << "Please input the first number. a= ";
+  // Declare an integer variable and read a value from the keyboard
+  int a;
+  std::cin >> a;
+  std::cout << "Please input the second number. b= ";
+  // Same thing for the second number
+  int b;
+  std::cin >> b;
 
-	// Compute and print the product;
-	std::cout << "The product of " << a << " and " << b << " is: "
-			  << a * b << std::endl;
+  // Compute and print the product;
+  std::cout << "The product of " << a << " and " << b << " is: " << a * b
+            << std::endl;
 
-	return 0;
+  return 0;
 }
