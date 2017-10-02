11ccc10473624e8614229eccae720d742ebeab53
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2017-10-02 09:48:46 +0200

    exercises of the day

---
+++ exercise_pdfs/exo-gdb-sources/alloc.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int get_size();
+
+void init(int array[]) {
+
+  int size = get_size();
+
+  for (int i = 1; i < size; ++i) {
+    array[i] = 0;
+  }
+}
+
+int main(int argc, char *argv[]) {
+  int size = 10;
+  int array[size];
+  int a = 1234;
+
+  std::cout << a << std::endl;
+  init(array);
+  std::cout << a << std::endl;
+
+  return 0;
+}
+
+int get_size() { return 20; }
